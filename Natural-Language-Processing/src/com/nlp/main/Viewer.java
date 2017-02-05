afe5b308412aa924646a06fa39e4617da806f90f
Suhas <Suhas@LAPTOP-JDR34VUE>
2017-02-05 19:02:50 +0800

    Create main user-interface

--- Natural-Language-Processing/src/com/nlp/main/Viewer.java
+++ Natural-Language-Processing/src/com/nlp/main/Viewer.java
@@ -1,6 +1,106 @@
 package com.nlp.main;
 
+import java.awt.BorderLayout;
+import java.awt.ComponentOrientation;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
 public class Viewer {
+	JLabel labelFile;
+	
+	JTextField txtInput;
+	JTextArea txtResults;
+	JButton btnLoad;
+	JButton btnBrowse;
+	
+	JFrame mainFrame;
+	JPanel mainPanel;
+	
+	public Viewer(){
+		
+		createGUI();
+	}
+	
+	public void createGUI(){
+	
+		mainFrame = new JFrame("NLP Visualizer");
+		mainFrame.setSize(new Dimension(400, 400));
+		mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent windowEvent){
+				System.exit(0);
+			}
+		});
+		
+		mainPanel = new JPanel();
+		mainPanel.setLayout(new BorderLayout());
+		
+		JPanel txtPanel = new JPanel(new FlowLayout());
+		labelFile = new JLabel("Input File");
+		
+		txtInput = new JTextField();
+		txtInput.setColumns(20);
+		txtInput.setSize(new Dimension(200,40));
+		
+		
+		btnBrowse = new JButton("Browse");
+		btnBrowse.setSize(new Dimension(40,40));
+		btnBrowse.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				final JFileChooser fileChooser = new JFileChooser();
+					int returnValue = fileChooser.showOpenDialog(mainFrame);
+					if(returnValue == JFileChooser.APPROVE_OPTION){
+						File selectedFile = fileChooser.getSelectedFile();
+						
+					}
+				}
+			
+			
+		});
+		
+		
+		btnLoad = new JButton("Load");
+		btnLoad.setSize(new Dimension(40, 40));
+		
+		txtPanel.add(labelFile);
+		txtPanel.add(txtInput);
+		txtPanel.add(btnBrowse);
+		txtPanel.add(btnLoad);
+		txtPanel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		
+		mainFrame.add(txtPanel, BorderLayout.NORTH);
+		
+		txtResults = new JTextArea();
+		txtResults.setSize(new Dimension(300, 300));
+		mainFrame.add(txtResults, BorderLayout.CENTER);
+		
+		mainFrame.pack();
+		mainFrame.setVisible(true);
+		
+		
+	}
+	
+	
+	public static void main(String[] args){
+		Viewer viewer = new Viewer();
+	}
 	
 	
 
